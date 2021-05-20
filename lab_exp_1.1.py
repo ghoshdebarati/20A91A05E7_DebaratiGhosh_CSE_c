ec286f44d9a9cc51b319f67a4b725590eac658ae
ghoshdebarati <84309001+ghoshdebarati@users.noreply.github.com>
2021-05-20 14:03:12 +0530

    Add files via upload

---
+++ lab_exp_1.1.py
@@ -0,0 +1,15 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input('enter the first number'))
+enter the first number 4
+>>> b=int(input('enter the second number'))
+enter the second number 3
+>>> c=int(input('enter the third number'))
+enter the third number 5
+>>> s=a+b+c
+>>> t=s/3
+>>> print('the sum is ',s)
+the sum is  12
+>>> print('the average is',t)
+the average is 4.0
+>>> 
