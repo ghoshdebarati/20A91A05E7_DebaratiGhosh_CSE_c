bbb1ea21c973c981fef3c3cd24bcf6690b767878
ghoshdebarati <84309001+ghoshdebarati@users.noreply.github.com>
2021-05-24 20:03:24 +0530

    Add files via upload

---
+++ odd_num.py
@@ -0,0 +1,21 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input('enter the first no.'))
+enter the first no. 45
+>>> b=int(input('enter the second no'))
+enter the second no 66
+>>> if a%2!=0:
+	print('odd')
+else :
+	print('even')
+
+	
+odd
+>>> if b%2==0:
+	print('not a odd number')
+else :
+	print('odd')
+
+	
+not a odd number
+>>> 
