81a1fc03cff8481d0465a5e04add085ddffa9d08
ghoshdebarati <84309001+ghoshdebarati@users.noreply.github.com>
2021-05-25 14:27:44 +0530

    Add files via upload

---
+++ eliff.py
@@ -0,0 +1,16 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input('enter the no.'))
+enter the no. 70
+>>> if a%2==0 :
+	print('the number is divisible by 2')
+elif a%3==0:
+	print('the number is divisible by 3')
+elif a%5==0:
+	print('the number is divisible by 5')
+else:
+	print('the no is not divisible by any of the numbers')
+
+	
+the number is divisible by 2
+>>> 
