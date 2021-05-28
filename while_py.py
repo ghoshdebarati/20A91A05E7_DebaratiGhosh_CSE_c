f282e812eaf6478739f87058527c86cc59a2f40e
ghoshdebarati <84309001+ghoshdebarati@users.noreply.github.com>
2021-05-28 23:15:28 +0530

    Add files via upload

---
+++ while_py.py
@@ -0,0 +1,17 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input('enter the end value'))
+enter the end value 9
+>>> b=int(input('enter the start value'))
+enter the start value 1
+>>> c=int(input('enter the step value'))
+enter the step value 3
+>>> while b<a:
+	print(b)
+	b=b+c
+
+	
+1
+4
+7
+>>> 
