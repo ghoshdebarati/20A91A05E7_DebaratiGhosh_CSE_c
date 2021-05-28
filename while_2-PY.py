6ffc3afb97de340453181c1b48a931d2dc125700
ghoshdebarati <84309001+ghoshdebarati@users.noreply.github.com>
2021-05-28 23:26:02 +0530

    Add files via upload

---
+++ while_2-PY.py
@@ -0,0 +1,18 @@
+a=int(input('enter the end value'))
+b=int(input('enter the start value'))
+c=int(input('enter the step value'))
+while b>a:
+	print(b)
+	b=b-c
+output
+enter the end value 1
+enter the start value9
+enter the step value1 
+9
+8
+7
+6
+5
+4
+3
+2
