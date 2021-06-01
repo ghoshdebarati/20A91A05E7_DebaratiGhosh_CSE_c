5369573719c1176c49f64eb3a1134573774c537e
ghoshdebarati <84309001+ghoshdebarati@users.noreply.github.com>
2021-06-01 13:38:15 +0530

    Add files via upload

---
+++ factors_py.py
@@ -0,0 +1,21 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n=int(input('enter the number'))
+enter the number 100
+>>> i=1
+>>> while i<=n:
+	if n%i==0:
+		print(i)
+	i=i+1
+
+	
+1
+2
+4
+5
+10
+20
+25
+50
+100
+>>> 
