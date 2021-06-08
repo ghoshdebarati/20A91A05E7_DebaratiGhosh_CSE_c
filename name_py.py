6f23c958df5bef1ec8cac85b6dc2c316cf20ad78
ghoshdebarati <84309001+ghoshdebarati@users.noreply.github.com>
2021-06-08 21:15:06 +0530

    Add files via upload

---
+++ name_py.py
@@ -0,0 +1,8 @@
+n=input('enter the name')
+b=int(input('enter the numbers of time the name is to be printed'))
+c=n*b
+print(c)
+output:-
+enter the name nayan
+enter the numbers of time the name is to be printed 2
+ nayan nayan
